44b499ca6cf0e09f03d12d092750de65603b6d32
rafael <mmrafaelss@gmail.com>
2025-06-13 12:49:17 -0600

    Makes it Cron compatible and changes commands used
    Deletes while, changes command for showing notification, changes icon and sound

--- batteryfull.sh
+++ batteryfull.sh
@@ -1,13 +1,31 @@
-#!/usr/bin/env bash
-while true
-do
-    export DISPLAY=:0.0
-    battery_percent=$(acpi -b | grep -P -o '[0-9]+(?=%)')
-    if on_ac_power; then
-        if [ "$battery_percent" -gt 95 ]; then
-            notify-send -i "$PWD/batteryfull.png" "Battery full." "Level: ${battery_percent}% "
-            paplay /usr/share/sounds/ubuntu/ringtones/Alarm\ clock.ogg
-        fi
-    fi
-    sleep 300 # (5 minutes)
-done
+#!/bin/bash
+
+
+
+#ANTES TENÍA: !/usr/bin/env bash
+
+
+icon="/usr/share/icons/Pop/32x32/emblems/emblem-default.svg"
+#/usr/share/icons/Pop/32x32/categories/cs-power.svg
+#"/usr/share/icons/Pop/32x32/devices/battery.svg" \
+sound="/usr/share/sounds/Pop/stereo/notification/system-ready.oga"
+
+export DISPLAY=:0.0
+actual_battery_percent=$(acpi -b | grep -P -o '[0-9]+(?=%)')
+is_plugged_in=$(cat /sys/class/power_supply/AC/online)
+if [ $is_plugged_in = 1 ] && [ "$actual_battery_percent" -gt 25 ]; then
+	#echo $sound > /tmp/desconectar.txt
+	/bin/gdbus call --session --dest org.freedesktop.Notifications \
+		--object-path /org/freedesktop/Notifications \
+		--method org.freedesktop.Notifications.Notify \
+		"Battery Full Notification" "0"\
+		$icon \
+		"Battery full" \
+		"The battery is <b>${actual_battery_percent}%</b> full." \
+		"[]" "{}" \
+		5000
+	/bin/paplay $sound
+	/bin/paplay $sound
+fi
+
+#gdbus call --session --dest org.freedesktop.Notifications --object-path /org/freedesktop/Notifications --method org.freedesktop.Notifications.Notify "YourAppName" "0" "Icon Path" "Notification Title" "Notification Message" "[]" "{}" 1500
