c95c33f28f910eb04a4355bd498508fd0ae61075
rafael <mmrafaelss@gmail.com>
2025-06-13 18:51:23 -0600

    Establece porcentaje en 99 y cambia comandos

--- batteryfull.sh
+++ batteryfull.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-
-
-#ANTES TENÍA: !/usr/bin/env bash
-
-
 icon="/usr/share/icons/Pop/32x32/emblems/emblem-default.svg"
 #/usr/share/icons/Pop/32x32/categories/cs-power.svg
 #"/usr/share/icons/Pop/32x32/devices/battery.svg" \
@@ -13,17 +7,16 @@ sound="/usr/share/sounds/Pop/stereo/notification/system-ready.oga"
 export DISPLAY=:0.0
 actual_battery_percent=$(acpi -b | grep -P -o '[0-9]+(?=%)')
 is_plugged_in=$(cat /sys/class/power_supply/AC/online)
-if [ $is_plugged_in = 1 ] && [ "$actual_battery_percent" -gt 25 ]; then
-	#echo $sound > /tmp/desconectar.txt
+if [ $is_plugged_in = 1 ] && [ "$actual_battery_percent" -gt 99 ]; then
 	/bin/gdbus call --session --dest org.freedesktop.Notifications \
-		--object-path /org/freedesktop/Notifications \
-		--method org.freedesktop.Notifications.Notify \
-		"Battery Full Notification" "0"\
-		$icon \
-		"Battery full" \
-		"The battery is <b>${actual_battery_percent}%</b> full." \
-		"[]" "{}" \
-		5000
+	    	--object-path /org/freedesktop/Notifications \
+	    	--method org.freedesktop.Notifications.Notify \
+	    	"Battery Full Notification" "0"\
+	    	$icon \
+	    	"Battery full" \
+	    	"The battery is <b>${actual_battery_percent}%</b> full." \
+	    	"[]" "{}" \
+	    	5000
 	/bin/paplay $sound
 	/bin/paplay $sound
 fi
